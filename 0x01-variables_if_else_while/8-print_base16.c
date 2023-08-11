a0f57f66a44c2e1c8af0c5bd5f15b4c746642c7f
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-11 17:06:48 +0200

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+int a = 0;
+while (d < 10)
+{
+putchar(d + '0');
+d++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
