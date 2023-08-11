70f0ef9a02f96ef655ce2161f94f3d0fbbd9bf8d
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-11 19:49:40 +0200

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@ for (hundreds = '0'; hundreds <= '9'; hundreds++)
 for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
+{
 if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 {
 putchar(hundreds);
@@ -23,6 +24,7 @@ if (!(ones == '9' && hundreds == '7' && tens == '8'))
 {
 putchar(',');
 putchar(' ');
+}
 }
 }
 }
