a9fb42de00d6041998f4554c3fb7b0fdf98fddbd
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-17 09:01:35 -0700

    sline

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,13 +6,13 @@
  */
 void print_square(int size)
 {
-int i, y;
+int i, x;
 
 if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++)
+for (x = 0; x < size; x++)
 {
 _putchar('#');
 }
