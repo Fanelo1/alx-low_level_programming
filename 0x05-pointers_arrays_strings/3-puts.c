f51e7d8e942109f599c808439fbe58e4964dbe11
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-21 08:54:32 -0700

    sweeps

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - print string and new line
+ * @str: prints string
+ */
+void _puts(char *str)
+{
+int i;
+for ( i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
