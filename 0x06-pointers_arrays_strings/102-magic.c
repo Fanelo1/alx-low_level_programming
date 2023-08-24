62c81d85055822654d554d61aaec8a9f0b38e300
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-24 04:52:42 -0700

    oneline

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
-
-int main(void)
-{
-int n;
-int a[5];
-int *p;
-a[2] = 1024;
-p = &n;
-/*
+/**
+* main - print numbers
 * write your line of code here...
 * Remember:
 * - you are not allowed to use a
@@ -15,6 +8,15 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
