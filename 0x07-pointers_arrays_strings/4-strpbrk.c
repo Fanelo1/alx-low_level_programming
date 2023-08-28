dde8d3e2b782882d2206163b18c69fa5079e8100
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-28 04:46:27 -0700

    findall

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/*
+ * _strbrk - seach any byte string
+ * @s: input
+ * @accept: input
+ * Return: 0 (seccess)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				return (s);
+			}
+			s++;
+		}
+	}
+	return ('\0');
+}
