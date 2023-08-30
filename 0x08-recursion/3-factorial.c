43fdea048b2f56db4930832519ebba6f04909b2c
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-30 17:45:11 +0200

    two

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - get factor number
+ * @n: factor
+ * Return: factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n);
+}
