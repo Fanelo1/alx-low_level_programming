b6957ff33e46f80bf24e1f7e16dcea667ebdf546
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-30 18:25:30 +0200

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * sqrt_a - return square root
+ * @n: input num
+ * @c: itarator
+ * Return: sqaure root or -1
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > 4)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1)
+}
+/**
+ * _sqrt_recursion - return the root
+ * @n: input
+ * Return: natural root
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt(n, 0));
+}
