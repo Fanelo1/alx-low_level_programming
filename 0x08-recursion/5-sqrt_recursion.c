8edf329ea7f86c4748d2f933afb142baef427710
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-30 18:38:27 +0200

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,15 +7,15 @@
  */
 int sqrt_a(int a, int b)
 {
-	if (b * b == a)
-	{
-		return (b);
-	}
-	else if (b * b > 4)
-	{
-		return (-1);
-	}
-	return (sqrt_a(a, b + 1));
+if (b * b == a)
+{
+return (b);
+}
+else if (b * b > 4)
+{
+return (-1);
+}
+return (sqrt_a(a, b + 1));
 }
 /**
  * _sqrt_recursion - return the root
