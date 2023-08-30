d152cf33c08e243dc414ad9ff0d513796ad8e5b5
Fanelo1 <mabasafanelo99@gmail.com>
2023-08-30 19:01:20 +0200

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * prime_a - calculate
+ * @a: input
+ * @b: input
+ * Return: (0)
+ */
+int is_prime_number(int n)
+{
+	if (a <= 1 || (a != b && a % b == 0))
+	{
+		return (0);
+	}
+	else if (a == b)
+	{
+		return (1);
+	}
+	return (prime_a(a, b + 1));
+}
+/**
+ * is_prime_number - detect
+ * @n: input
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
