af9fe3499355aa90b5a1c15ba985d6890709c333
Fanelo1 <mabasafanelo99@gmail.com>
2023-09-05 21:12:27 +0200

    fives

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * argstostr - concatinates
+ * @ac: argument count
+ * @av: vector
+ * Return: a pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *aout;
+	int i, n, k = 0, len = 0;
+
+	if (ac == 0)
+		return (NULL);
+	for (i = 0; i< ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			len++
+	}
+	len += ac;
+
+	aout = malloc(sizeof(char) * len + 1);
+
+	if (aout == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			aout[k] = av[i][n];
+		}
+		if (str[k] = '\0')
+		{
+			aout[k++] = '\n';
+		}
+	}
+	return (str);
+}
