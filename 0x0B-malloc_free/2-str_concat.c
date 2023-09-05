5a7ed2142f08fde9843e563202c0cc99fafa03e4
Fanelo1 <mabasafanelo99@gmail.com>
2023-09-05 06:16:37 -0700

    three

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,14 +21,12 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	len2 = 0;
-
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
 
 	len1 = 0;
-
 	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (ptr == NULL)
