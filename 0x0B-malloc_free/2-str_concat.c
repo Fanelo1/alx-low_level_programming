c989d0a2e0e60f3681d62aad1271f2e8e07c3ad4
Fanelo1 <mabasafanelo99@gmail.com>
2023-09-05 06:13:50 -0700

    three

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * str_concat - concertinate
+ * @s1: first
+ * @s2: second
+ * Return: pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1, len2, i, j;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len2 = 0;
+
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+
+	len1 = 0;
+
+	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; j <= len2; j++)
+	{
+		ptr[i] = s2[j];
+		i++;
+	}
+	return (ptr);
+}
